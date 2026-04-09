xiaomi-mediatek-devs/android_kernel_xiaomi_mt6877

drivers: aw8697_haptic: Suppress spammy verbose logging
Change-Id: I887eda6249a0593b0dd57a8d48b2f1eb808f6e59
Signed-off-by: bengris32 <bengris32@protonmail.ch>

drivers/input/misc/aw8697_haptic/aw_haptic.h
@@ -13,10 +13,10 @@
  * print information control
  *******************************************/
 #define aw_err(format, ...) \
-			pr_err("[haptic_hv]" format, ##__VA_ARGS__)
+			pr_debug("[haptic_hv]" format, ##__VA_ARGS__)
 
 #define aw_info(format, ...) \
-			pr_info("[haptic_hv]" format, ##__VA_ARGS__)
+			pr_debug("[haptic_hv]" format, ##__VA_ARGS__)
 
 #define aw_dbg(format, ...) \
 			pr_debug("[haptic_hv]" format, ##__VA_ARGS__)

A1cInt/alc_android_kernel_xiaomi_mt6895

(cherry picked from commit c2e7728656634ee0c63582305fd2ffc8d4e6521b)

drivers/input/misc/aw8697_haptic/aw_haptic.h
@@ -13,10 +13,10 @@
  * print information control
  *******************************************/
 #define aw_err(format, ...) \
-			pr_err("[awinic_haptic]" format, ##__VA_ARGS__)
+			pr_debug("[awinic_haptic]" format, ##__VA_ARGS__)
 
 #define aw_info(format, ...) \
-			pr_info("[awinic_haptic]" format, ##__VA_ARGS__)
+			pr_debug("[awinic_haptic]" format, ##__VA_ARGS__)
 
 #define aw_dbg(format, ...) \
 			pr_debug("[awinic_haptic]" format, ##__VA_ARGS__)